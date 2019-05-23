52ed7229b882ad2f5476d2f711c5a6cdee617a32
HYL <haha505254@gmail.com>
2019-05-23 12:32:53 +0800

    Control our tanks to move to eight different directions

---
+++ src/main/java/com/haha505254/tankwar/Direction.java
@@ -0,0 +1,13 @@
+package com.haha505254.tankwar;
+
+public enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGTH,
+    UPLEFT,
+    UPRIGHT,
+    DOWNLEFT,
+    DOWNRIGHT
+
+}
--- src/main/java/com/haha505254/tankwar/GameClient.java
+++ src/main/java/com/haha505254/tankwar/GameClient.java
@@ -2,10 +2,16 @@ package com.haha505254.tankwar;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class GameClient extends JComponent {
 
+    private Tank playertank;
+
     private GameClient(){
+        this.playertank = new Tank(400,100,Direction.DOWN);
+
         this.setPreferredSize(new Dimension(800,600));
 
     }
@@ -13,20 +19,41 @@ public class GameClient extends JComponent {
     @Override
     protected void paintComponent(Graphics g) {
 
-        g.drawImage(new ImageIcon("assets/images/tankD.gif").getImage(),400,100,null);
+        playertank.draw(g);
     }
 
     public static void main(String[] args) {
         JFrame frame=new JFrame();
         frame.setTitle("坦克小遊戲");
         frame.setIconImage(new ImageIcon("assets/images/icon.png").getImage());
-        GameClient client=new GameClient();
+        final GameClient client=new GameClient();
         client.repaint();
         frame.add(client);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
+
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                client.playertank.keyPressed(e);
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                client.playertank.keyReleased(e);
+            }
+        });
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        while (true){
+            client.repaint();
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
 
     }
 }
