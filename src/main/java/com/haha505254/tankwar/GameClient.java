0ad629022caac20bc6f3d4dbaeac6d5d651714c3
HYL <haha505254@gmail.com>
2019-05-23 13:26:46 +0800

    Set up enemy tanks

--- src/main/java/com/haha505254/tankwar/GameClient.java
+++ src/main/java/com/haha505254/tankwar/GameClient.java
@@ -4,13 +4,25 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameClient extends JComponent {
 
     private Tank playertank;
 
+    private List<Tank> enemyTanks ;
+
+
+
     private GameClient(){
         this.playertank = new Tank(400,100,Direction.DOWN);
+        this.enemyTanks = new ArrayList<>(12);
+        for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+            this.enemyTanks.add(new Tank(200+j*120,400+40*i,true,Direction.UP));
+            }
+        }
 
         this.setPreferredSize(new Dimension(800,600));
 
@@ -20,6 +32,9 @@ public class GameClient extends JComponent {
     protected void paintComponent(Graphics g) {
 
         playertank.draw(g);
+        for(Tank tank:enemyTanks){
+            tank.draw(g);
+        }
     }
 
     public static void main(String[] args) {
