aec3af19ecbc1b0ebd258f689c217c399f885646
HYL <haha505254@gmail.com>
2019-05-23 09:50:38 +0800

    Display main interface and manufacture of our tanks

---
+++ src/main/java/com/haha505254/tankwar/GameClient.java
@@ -0,0 +1,32 @@
+package com.haha505254.tankwar;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameClient extends JComponent {
+
+    private GameClient(){
+        this.setPreferredSize(new Dimension(800,600));
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+
+        g.drawImage(new ImageIcon("assets/images/tankD.gif").getImage(),400,100,null);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame=new JFrame();
+        frame.setTitle("坦克小遊戲");
+        frame.setIconImage(new ImageIcon("assets/images/icon.png").getImage());
+        GameClient client=new GameClient();
+        client.repaint();
+        frame.add(client);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+    }
+}
